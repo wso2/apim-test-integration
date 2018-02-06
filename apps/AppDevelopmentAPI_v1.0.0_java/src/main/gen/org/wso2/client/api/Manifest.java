4422a02c40d9fc44fb7447485d3f1f6d070051ae
chaminda@wso2.com <chaminda@wso2.com>
2018-02-07 00:02:04 +0530

    Revert "update with scenario04 scripts and common scripts structure"
    This reverts commit 36bca64113145eda69461bc11fafbaf283b69b1e.

--- apps/AppDevelopmentAPI_v1.0.0_java/src/main/gen/org/wso2/client/api/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.wso2.client.api;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
--- apps/AppDevelopmentAPI_v1.0.0_java/src/main/gen/org/wso2/client/api/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.wso2.client.api;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
