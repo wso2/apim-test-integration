2dc693cf4164b98634c68c581e642873ff359f8e
asma <azinneera@gmail.com>
2018-05-03 15:20:57 +0530

    Fix errors in scenario03 and write results to xml

---
+++ scenario03/jmeter/03-pre-scenario-steps.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+serverHost=$serverHost
+serverPort=$serverPort
+pubHost=$pubHost
+pubPort=$pubPort
+storeHost=$storeHost
+storePort=$storePort
+gwMgrHost=$gwMgrHost
+gwMgrPort=$gwMgrPort
+gwWrkHost=$gwWrkHost
+gwWrkPort=$gwWrkPort
+MSF4JHost=$MSF4JHost
+MSF4JPort=$MSF4JPort
+
+prgdir=$(dirname "$0")
+scriptPath=$(cd "$prgdir"; pwd)
+
+echo "working directory : "$scriptPath
+
+#updating jmeter properties - user.properties
+sed -i "s|^\(serverHost\s*=\s*\).*\$|\1${serverHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(serverPort\s*=\s*\).*\$|\1${serverPort}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(pubHost\s*=\s*\).*\$|\1${pubHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(pubPort\s*=\s*\).*\$|\1${pubPort}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(storeHost\s*=\s*\).*\$|\1${storeHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(storePort\s*=\s*\).*\$|\1${storePort}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(gwMgrHost\s*=\s*\).*\$|\1${gwMgrHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(gwMgrPort\s*=\s*\).*\$|\1${gwMgrPort}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(gwWrkHost\s*=\s*\).*\$|\1${gwWrkHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(gwWrkPort\s*=\s*\).*\$|\1${gwWrkPort}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(MSF4JHost\s*=\s*\).*\$|\1${MSF4JHost}|" $scriptPath/../resources/user.properties
+sed -i "s|^\(MSF4JPort\s*=\s*\).*\$|\1${MSF4JPort}|" $scriptPath/../resources/user.properties
+#run base-setup.sh
+source $scriptPath/../base-setup.sh > $scriptPath/basesetup.log
+
