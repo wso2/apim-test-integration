963a5406df7d494d5d817650ac28acd21386e0ea
chaminda@wso2.com <chaminda@wso2.com>
2018-06-28 12:16:38 +0530

    improvements to do-run scripts

--- integration-tests/do-run.py
+++ integration-tests/do-run.py
@@ -1,9 +1,15 @@
+
+
 # importing required modules
 import yaml
 import subprocess
 import wget
 import logging
 import inspect
+import os
+import shutil
+import pymysql
+import sqlparse
 from pathlib import Path
 
 git_repo_url = None
@@ -18,6 +24,10 @@ db_type = None
 database_config = None
 log_file_name = 'integration.log'
 target_path = None
+DB_CARBON_DB = 'WSO2_CARBON_DB'
+DB_AM_DB = 'WSO2AM_DB'
+DB_STAT_DB = 'WSO2AM_STATS_DB'
+DB_MB_DB = 'WSO2_MB_STORE_DB'
 
 def function_logger(file_level, console_level = None):
     function_name = inspect.stack()[1][3]
@@ -74,28 +84,6 @@ def download_file(url, destination):
     """
     wget.download(url, destination)
 
-def run_sqlserver_commands(sql_host, sql_user, sql_pass, sql_query):
-    """Run SQL_SERVER commands using sqlcmd utility.
-    """
-    subprocess.call(['sqlcmd', '-S', sql_host, '-U', sql_user, '-P', sql_pass, '-Q', sql_query])
-
-def run_mysql_commands(host, user, passwrd, query):
-    """Run mysql commands using mysql client.
-    """
-    subprocess.call(['mysql', '-H', host, '-u', user, '-p', passwrd, '-e', sql_query])
-
-def run_sqlserver_script_file(sql_host, sql_user, sql_pass, db_name, script_path):
-    """Run SQL_SERVER script file on a provided database.
-    """
-    #sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -d $UM_DB -i $DB_SCRIPTS_PATH/mssql.sql
-    subprocess.call(['sqlcmd', '-S', sql_host, '-U', sql_user, '-P', sql_pass, '-d', db_name, '-i', script_path])
-
-def run_mysql_script_file(sql_host, sql_user, sql_pass, db_name, script_path):
-    """Run MYSQL db script file on a provided database.
-    """
-    #sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -d $UM_DB -i $DB_SCRIPTS_PATH/mssql.sql
-    #TODO: implement the code
-
 def get_db_hostname(url, db_type):
     """Retreive db hostname from jdbc url
     """
@@ -106,34 +94,126 @@ def get_db_hostname(url, db_type):
         hostname = url.split(':')[2].replace("//","")
     return hostname
 
+def run_sqlserver_commands(sql_host, sql_user, sql_pass, sql_query):
+    """Run SQL_SERVER commands using sqlcmd utility.
+    """
+    subprocess.call(['sqlcmd', '-S', sql_host, '-U', sql_user, '-P', sql_pass, '-Q', sql_query])
+
+def get_mysql_connection(dbName=None):
+    if dbName is not None:
+        conn = pymysql.connect(host=get_db_hostname(database_config['url'], 'MYSQL'), user=database_config['user'], passwd=database_config['passwd'], db=dbName)
+    else:
+        conn = pymysql.connect(host=get_db_hostname(database_config['url'], 'MYSQL'), user=database_config['user'], passwd=database_config['passwd'])
+    return conn
+
+def run_mysql_commands(query):
+    """Run mysql commands using mysql client when db name not provided.
+    """
+    #subprocess.call(['mysql', '-H', host, '-u', user, '-p', passwrd, '-e', query])
+    conn = get_mysql_connection()
+    conectr = conn.cursor()
+    conectr.execute(query)
+    conn.close()
+
+def run_sqlserver_script_file(sql_host, sql_user, sql_pass, db_name, script_path):
+    """Run SQL_SERVER script file on a provided database.
+    """
+    #sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -d $UM_DB -i $DB_SCRIPTS_PATH/mssql.sql
+    subprocess.call(['sqlcmd', '-S', sql_host, '-U', sql_user, '-P', sql_pass, '-d', db_name, '-i', script_path])
+
+def run_mysql_script_file(db_name, script_path):
+    """Run MYSQL db script file on a provided database.
+    """
+    conn = get_mysql_connection(db_name)
+    conectr = conn.cursor()
+
+    sql = open(script_path).read()
+    sql_parts = sqlparse.split( sql )
+    for sql_part in sql_parts:
+        if sql_part.strip() ==  '':
+            continue 
+        conectr.execute( sql_part )
+    conn.close()
+
+
 def copy_file(os_type, source, target):
     if(os_type == "WINDOWS"):
-        subprocess.call(['copy', source, target])
+        source = make_windows_path(source)
+        target = make_windows_path(target)
+        shutil.copy(source, target)
     elif (os_type == "LINUX"):
-        subprocess.call(['cp', source, target])
+        shutil.copy(source, target)
 
-def setup_databases():
+def make_windows_path(win_path, encoding=None):
+    path = os.path.abspath(win_path)
+    if path.startswith("\\\\"):
+        path = "\\\\?\\UNC\\" + path[2:]
+    else:
+        path = "\\\\?\\" + path
+    return path
+
+def setup_databases(script_path):
     """Create required databases.
-    """
-    read_config_file()
-    #create databases
-    db_name = database_config['url']
-    
+    """  
     print('Creating databases...')
     for database in database_names:
-        if database == 'WSO2_CARBON_DB':
+        if database == DB_CARBON_DB:
             if db_type == 'MSSQL':
                 #create database
                 run_sqlserver_commands(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], 'CREATE DATABASE WSO2_CARBON_DB')
                 #manipulate script path
-                run_sqlserver_script_file(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], database, )
-                
+                scriptPath = script_path / 'mssql.sql'
+                #run db scripts
+                run_sqlserver_script_file(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], database, str(scriptPath))              
             elif db_type == 'MYSQL':
-                #TODO: mysql implementation
-                testval = 'testvalmysql'
+                scriptPath = script_path / 'mysql5.7.sql'
+                #create database
+                run_mysql_commands('CREATE DATABASE IF NOT EXISTS {0};'.format(database))
+                #run db script
+                run_mysql_script_file(database, str(scriptPath))
+
             elif db_type == 'ORACLE':
                 #TODO: oracle implementation
-                testval = 'testval'
+                testval = 'testValue'
+                pass
+        elif database == DB_AM_DB:
+            if db_type == 'MSSQL':
+                #create database
+                run_sqlserver_commands(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], 'CREATE DATABASE WSO2AM_DB')
+                #manipulate script path
+                scriptPath = script_path / 'apimgt/mssql.sql'
+                #run db scripts
+                run_sqlserver_script_file(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], database, str(scriptPath))
+            elif db_type == 'MYSQL':
+                scriptPath = script_path / 'apimgt/mysql5.7.sql'
+                #create database
+                run_mysql_commands('CREATE DATABASE IF NOT EXISTS {0};'.format(database))
+                #run db script
+                run_mysql_script_file(database, str(scriptPath))
+            elif db_type == 'ORACLE':
+                pass
+        elif database == DB_STAT_DB:
+            if db_type == 'MSSQL':
+                #create database
+                run_sqlserver_commands(get_db_hostname(database_config['url'],'MSSQL'), database_config['user'], database_config['passwd'], 'CREATE DATABASE WSO2AM_STATS_DB')               
+            elif db_type == 'MYSQL':
+                #create database
+                run_mysql_commands('CREATE DATABASE IF NOT EXISTS {0};'.format(database))
+            elif db_type == 'ORACLE':
+                pass
+        elif database == DB_MB_DB:
+            if db_type == 'MSSQL':
+                pass
+            elif db_type == 'MYSQL':
+                #create database
+                run_mysql_commands('CREATE DATABASE IF NOT EXISTS {0};'.format(database))
+                #manipulate script path
+                scriptPath = script_path / 'mb-store/mysql-mb.sql'
+                #run db scripts
+                run_mysql_script_file(database, str(scriptPath))
+            elif db_type == 'ORACLE':
+                pass
+
 
 def main():
     try:
@@ -145,33 +225,35 @@ def main():
         logger.info('cloning repo done.')
 
         #build the product
-        #subprocess.call(['mvn', 'clean', 'install'], cwd='path/to/product/repo')
+        subprocess.call(['mvn', 'clean', 'install'], cwd='path/to/product/repo')
         
         #TODO: need to generate the url in correct way.
+        #xpath=https://wso2.org/jenkins/job/products/job/product-apim_2.x/lastRelease/api/xml?xpath=/*/artifact[1]/fileName
         url = jeinkins_build_url
         product_file_name = product_name + ".zip"
-        destination = Path(workspace_dir + "/" + product_file_name)
+
+        destination = Path(workspace_dir + "/storage/" + product_file_name)
 
         #download the last released pack from Jenkins
-        download_file(url, destination)
+        download_file(url, str(destination))
         logger.info('downloading the pack from Jenkins done.')
 
         #run python file for product configuration
-        #TODO: decide python file name and directory
-        #subprocess.call(['python', 'path/to/config/python.py'])
+        #subprocess.call(['python', 'Configure_Product.py'])
 
         #populate databases
-        setup_databases()
-        logger.info('database setting up done.')
+        script_path = Path(workspace_dir + "/storage/" + product_name + "/" + 'dbscripts')
+        setup_databases(script_path)
+        logger.info('Database setting up is done.')
 
         #create directory structure inside cloned repo and copy product distribution
         target_path = Path(workspace_dir + "/" + product_id + "/" + 'modules/distribution/product/target')
         target_path.mkdir(parents=True)
-        logger.info('target directory created.')
+        logger.info('Target directory created.')
 
         #copy the pack to target directory
         copy_file(os_type, destination, target_path)
-        logger.info('product distribution copied to ' + str(target_path))
+        logger.info('Product distribution copied to ' + str(target_path))
 
         #run integration tests
         integration_tests_path = Path(workspace_dir + "/" + product_id + "/" + 'modules/integration/tests-integration')
