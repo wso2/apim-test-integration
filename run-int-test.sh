04023ff3e8b379a6678ccc3c4945186716137d69
NishikaDeSilva <desilvanishika73@gmail.com>
2020-07-13 11:26:37 +0530

    Update resource content

--- run-int-test.sh
+++ run-int-test.sh
@@ -14,9 +14,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #----------------------------------------------------------------------------
-set -o xtrace
+set -o xtrace; set -e
 
-WORKING_DIR=$(pwd)
+TESTGRID_DIR=/opt/testgrid/workspace
 INFRA_JSON='infra.json'
 
 PRODUCT_REPOSITORY=$1
@@ -25,11 +25,11 @@ PRODUCT_NAME=$3
 PRODUCT_VERSION=$4
 
 PRODUCT_REPOSITORY_NAME=$(echo $PRODUCT_REPOSITORY | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
-PRODUCT_REPOSITORY_PACK_DIR="$WORKING_DIR/$PRODUCT_REPOSITORY_NAME/modules/distribution/product/target"
-INT_TEST_MODULE_DIR="$WORKING_DIR/$PRODUCT_REPOSITORY_NAME/modules/integration"
+PRODUCT_REPOSITORY_PACK_DIR="$TESTGRID_DIR/$PRODUCT_REPOSITORY_NAME/modules/distribution/product/target"
+INT_TEST_MODULE_DIR="$TESTGRID_DIR/$PRODUCT_REPOSITORY_NAME/modules/integration"
 
 # cloud formation properties
-CFN_PROP_FILE="${WORKING_DIR}/cfn-props.properties"
+CFN_PROP_FILE="${TESTGRID_DIR}/cfn-props.properties"
 JDK_TYPE=$(grep -w "JDK_TYPE" ${CFN_PROP_FILE} | cut -d"=" -f2)
 DB_TYPE=$(grep -w "DB_TYPE" ${CFN_PROP_FILE} | cut -d"=" -f2)
 CF_DB_VERSION=$(grep -w "CF_DB_VERSION" ${CFN_PROP_FILE} | cut -d"=" -f2)
@@ -80,19 +80,16 @@ function export_db_params(){
     
 }
 
+source /etc/environment
+
 log_info "Clone Product repository"
-git clone https://$PRODUCT_REPOSITORY --branch $PRODUCT_REPOSITORY_BRANCH
-
-mkdir -p $PRODUCT_REPOSITORY_PACK_DIR
-
-log_info "Copying product pack to Repository"
-mv $PRODUCT_NAME-$PRODUCT_VERSION-*.zip $PRODUCT_REPOSITORY_PACK_DIR/. -P ${PRODUCT_NAME}-${PRODUCT_VERSION}-*/repository/components/lib
+git clone https://$PRODUCT_REPOSITORY $TESTGRID_DIR/${PRODUCT_REPOSITORY_NAME} --branch $PRODUCT_REPOSITORY_BRANCH
 
 log_info "Exporting JDK"
-install_jdk ${JDK}
+install_jdk ${JDK_TYPE}
 
 db_file=$(jq -r '.jdbc[] | select ( .name == '\"${DB_TYPE}\"') | .file_name' ${INFRA_JSON})
-wget -q https://integration-testgrid-resources.s3.amazonaws.com/lib/jdbc/${DB_TYPE}.jar
+wget -q https://integration-testgrid-resources.s3.amazonaws.com/lib/jdbc/${db_file}.jar  -P $TESTGRID_DIR/${PRODUCT_NAME}-${PRODUCT_VERSION}-*/repository/components/lib
 
 sed -i "s|DB_HOST|${CF_DB_HOST}|g" ${INFRA_JSON}
 sed -i "s|DB_USERNAME|${CF_DB_USERNAME}|g" ${INFRA_JSON}
@@ -100,31 +97,12 @@ sed -i "s|DB_PASSWORD|${CF_DB_PASSWORD}|g" ${INFRA_JSON}
 sed -i "s|DB_NAME|${DB_NAME}|g" ${INFRA_JSON}
 
 export_db_params ${DB_TYPE}
-# case ${JDK_TYPE} in
-#     ADOPT_OPEN_JDK8)
-#         wget -q https://integration-testgrid-resources.s3.amazonaws.com/lib/jdk/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz
-#         install_jdk ADOPT_OPEN_JDK8 "OpenJDK8U-jdk_x64_linux_hotspot_8u252b09"
-#         export JAVA_HOME=/opt/ADOPT_OPEN_JDK8
-#         echo $JAVA_HOME
-#         ;;
-# esac
 
-# case ${DB_TYPE} in
-#     mysql)
-#         wget -q https://integration-testgrid-resources.s3.amazonaws.com/lib/jdbc/mysql-connector-java-5.1.49.jar -P ${PRODUCT_NAME}-${PRODUCT_VERSION}-*/repository/components/lib
-        
-#         export SHARED_DATABASE_DRIVER="com.mysql.jdbc.Driver"
-#         export SHARED_DATABASE_URL="jdbc:mysql://$CF_DB_HOST:3306/WSO2AM_COMMON_DB?autoReconnect=true&amp;useSSL=false"
-#         export SHARED_DATABASE_USERNAME="${CF_DB_USERNAME}"
-#         export SHARED_DATABASE_PASSWORD="${CF_DB_PASSWORD}"
-#         export SHARED_DATABASE_VALIDATION_QUERY="SELECT 1"
-        
-#         export API_MANAGER_DATABASE_DRIVER="com.mysql.jdbc.Driver"
-#         export API_MANAGER_DATABASE_URL="jdbc:mysql://$CF_DB_HOST:3306/WSO2AM_APIMGT_DB?autoReconnect=true&amp;useSSL=false"
-#         export API_MANAGER_DATABASE_USERNAME="${CF_DB_USERNAME}"
-#         export API_MANAGER_DATABASE_PASSWORD="${CF_DB_PASSWORD}"
-#         export API_MANAGER_DATABASE_VALIDATION_QUERY="SELECT 1"
-#         ;;
-# esac
+mkdir -p $PRODUCT_REPOSITORY_PACK_DIR
+
+log_info "Copying product pack to Repository"
+[ -f $TESTGRID_DIR/$PRODUCT_NAME-$PRODUCT_VERSION-*.zip ] && rm -f $TESTGRID_DIR/$PRODUCT_NAME-$PRODUCT_VERSION-*.zip
+cd $TESTGRID_DIR && zip -qr $PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_NAME-$PRODUCT_VERSION-*
+mv $TESTGRID_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_PACK_DIR/.
 
 cd $INT_TEST_MODULE_DIR  && mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
