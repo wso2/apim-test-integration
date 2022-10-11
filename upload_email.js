7419216e5b306a0c843ff63b1988d9599526c025
Chamika Sudusinghe <44718392+chamikasudusinghe@users.noreply.github.com>
2022-10-11 08:01:10 +0530

    emails removed till fix

--- upload_email.js
+++ upload_email.js
@@ -56,7 +56,7 @@ const sendMail = () => {
 
   var mailOptions = {
     from: "TestGrid Team <testgrid@wso2.com>",
-    to: "prasanna@wso2.com,vimukthi@wso2.com,rosens@wso2.com,nandika@wso2.com,dhanushka@wso2.com,bathiya@wso2.com,chamikas@wso2.com",
+    to: "chamikas@wso2.com",//"prasanna@wso2.com,vimukthi@wso2.com,rosens@wso2.com,nandika@wso2.com,dhanushka@wso2.com,bathiya@wso2.com,chamikas@wso2.com",
     subject: `WSO2 APIM 3.2.0 UI TESTS`,
     html: content
   }
