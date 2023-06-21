278bb5bdefbafdfd4002a5d399e6d0edddc0697c
Chamika Sudusinghe <44718392+chamikasudusinghe@users.noreply.github.com>
2023-06-21 16:36:37 +0530

    Revert "install other dependencies"
    This reverts commit 0702e7b41a55a7d656bc182b4a1f14ada947bb2e.

--- test.sh
+++ test.sh
@@ -104,7 +104,6 @@ sudo dpkg --configure -a
 curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm -v
-sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 export LC_CTYPE="en_US.UTF-8"
 cd $HOME
 npm install cypress
