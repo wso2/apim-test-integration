df62f0227c8bc734874c812b9ad00c0267abcfec
Chamika Sudusinghe <44718392+chamikasudusinghe@users.noreply.github.com>
2023-06-21 16:37:09 +0530

    Revert "removed addition npm installation script"
    This reverts commit cbe8e99cd21b02944064cc4df75b5aafbd7d78a1.

--- test.sh
+++ test.sh
@@ -101,7 +101,19 @@ sudo apt-get update -y
 sleep 300
 sudo killall apt apt-get dpkg
 sudo dpkg --configure -a
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+chmod 644 /usr/share/keyrings/nodesource.gpg
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+npm -v
+if [[ $? -ne 0 ]]
+then
+    echo "NPM exists and removing existing version."
+    sudo apt-get purge nodejs -y
+    sudo apt-get purge npm -y
+else
+    echo "NPM Deos NOT exists and installing existing version."
+fi
+
 wget https://nodejs.org/dist/v12.22.3/node-v12.22.3-linux-x64.tar.xz
 tar -xvf node-v12.22.3-linux-x64.tar.xz
 sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/node /usr/bin/node
