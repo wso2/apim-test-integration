2f14ba6996d3d89a4f9f0ebba6b10fabd7de54e1
Chamika Sudusinghe <44718392+chamikasudusinghe@users.noreply.github.com>
2023-06-21 16:36:53 +0530

    Revert "install npm frome nodesource"
    This reverts commit 71de0db9d80f79176a8c1d19ccf2df156d73db3d.

--- test.sh
+++ test.sh
@@ -101,10 +101,14 @@ sudo apt-get update -y
 sleep 300
 sudo killall apt apt-get dpkg
 sudo dpkg --configure -a
-curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm -v
+wget https://nodejs.org/dist/v12.22.3/node-v12.22.3-linux-x64.tar.xz
+tar -xvf node-v12.22.3-linux-x64.tar.xz
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/node /usr/bin/node
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npx /usr/bin/npx
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 export LC_CTYPE="en_US.UTF-8"
+npm -v
 cd $HOME
 npm install cypress
 npm install --save-dev cypress-file-upload
