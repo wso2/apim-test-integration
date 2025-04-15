2e9a623ab02197e2e849bb14b6cecd5dc3313adb
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-15 16:01:43 +0530

    Update newman command in main.sh to use correct endpoint and add additional environment variables

--- main.sh
+++ main.sh
@@ -76,11 +76,13 @@ environment_file=$tests_dir/tests-cases/profile-tests/APIM_Environment.postman_e
 
 operation_policy_file_path="$tests_dir/tests-cases/profile-tests/resources/operation-policy-testcase/changeHTTPMethod_v2.j2"
 
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/newman run "$collection_file" \
+newman run "$collection_file" \
   --environment "$environment_file" \
   --env-var "cluster_ip=${HOST_NAME}" \
-  --env-var "pizzashack_endpoint=https://wso2am-pattern-4-am-cp-service:9443/am/sample/pizzashack/v1/api/" \
+  --env-var "pizzashack_endpoint=https://apim-acp-wso2am-acp-service:9443/am/sample/pizzashack/v1/api/" \
   --env-var "operation_policy_file_path=$operation_policy_file_path" \
+  --env-var "portals_host=${PORTAL_HOST}" \
+  --env-var "gateway_host=${GATEWAY_HOST}" \
   --insecure \
   --reporters cli,junit \
   --reporter-junit-export newman-profile-results.xml
