803a28d5520bc8af1ece9a1475e01a9ec47d7331
Kavinda Rajapakse <kavindasr@gmail.com>
2026-02-17 10:14:02 +0530

    4.4.0 profile automation parallel builds (#310)
    * Update fargate profile name to include product version in cleanup and deploy scripts
    
    * Refactor nginx ingress controller installation logic to check for existence before applying changes
    
    * Add ingress hostname configuration for APIM and gateway in deployment scripts
    
    * Update fargate profile name to replace '.' with '_' in cleanup and deploy scripts
    
    * Temp comment nodegroup scale
    
    * Update fargate profile name to use SHORT_PRODUCT_VERSION in cleanup, deploy, and main scripts
    
    * Remove node group scaling command from deployment script
    
    * Add nodegroup
    
    * Remove node group scaling command and create fargate profile in deployment script
    
    * Refactor nginx ingress controller installation logic in deployment script
    
    * Revert "Refactor nginx ingress controller installation logic in deployment script"
    
    This reverts commit c533083014f6c59cc55349a5811a16db17be6494.
    
    * Revert "Remove node group scaling command and create fargate profile in deployment script"
    
    This reverts commit 5836ce6269cf03cc0ab3c58efd3db1514a02d6b0.

--- cleanup.sh
+++ cleanup.sh
@@ -29,7 +29,7 @@ echo "Uninstalling APIM in cluster."
 helm uninstall "${product_name}" -n="${kubernetes_namespace}" || true
 
 echo "Delete fargate profile."
-eksctl delete fargateprofile  --name "${product_name}-fargate-profile" --cluster "${EKS_CLUSTER_NAME}" --region ${EKS_CLUSTER_REGION}
+eksctl delete fargateprofile  --name "${product_name}-${SHORT_PRODUCT_VERSION}-fargate-profile" --cluster "${EKS_CLUSTER_NAME}" --region ${EKS_CLUSTER_REGION}
 
 cd "$workingdir"
 
