3ab6765723d5b4479c6f0046095d9f7914b72c39
Pasan Tennakoon <pasan96tennakoon@gmail.com>
2026-07-18 23:36:05 +0530

    Change helm install to use set-string for credentials (#344)

--- deploy.sh
+++ deploy.sh
@@ -182,8 +182,8 @@ fi
 updateLevelState='TESTING'
 
 helm install apim "kubernetes-apim/${path_to_helm_folder}" \
-    --set wso2.subscription.username=${WUM_USER} \
-    --set wso2.subscription.password=${WUM_PWD} \
+    --set-string "wso2.subscription.username=${WUM_USER}" \
+    --set-string "wso2.subscription.password=${WUM_PWD}" \
     --set wso2.subscription.updateLevelState=$updateLevelState \
     --set wso2.deployment.am.cp.db.hostname="$dbHost" \
     --set wso2.deployment.am.cp.db.port="$dbPort" \
